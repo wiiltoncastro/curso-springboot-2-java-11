bb59bb6366dc63f96fc7fbf113849b1b8da26f3e
Wilton Castro <wilton_alt@hotmail.com>
2021-03-19 09:34:38 -0300

    Projeto Criado

---
+++ src/test/java/com/educandoweb/workshop/Course1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
