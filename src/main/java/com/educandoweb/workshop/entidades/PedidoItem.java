880f7ee950023e6651a2ea28a5860580a307ed89
Wilton Castro <wilton_alt@hotmail.com>
2021-03-23 11:29:35 -0300

    Entidade PedidoItem

---
+++ src/main/java/com/educandoweb/workshop/entidades/PedidoItem.java
@@ -0,0 +1,87 @@
+package com.educandoweb.workshop.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.educandoweb.workshop.entidades.pk.PedidoItemPk;
+
+@Entity
+public class PedidoItem implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private PedidoItemPk id;
+	
+	private Integer quantidade;
+	private Double preco;
+	
+	//construtor
+	public PedidoItem() {
+		super();
+	}
+	public PedidoItem(Pedido pedido, Produto produto, Integer quantidade, Double preco) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+	
+	//gets e sets
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PedidoItem other = (PedidoItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
